4a56b31da422ca713b8fdb3412830d87c58c0f8c
sajid <sajidshaikh12543@gmail.com>
2024-07-30 14:00:31 +0530

    29/7/24

--- lib/Provider/pixaBay_provider.dart
+++ lib/Provider/pixaBay_provider.dart
@@ -1,24 +1,28 @@
 import 'package:flutter/foundation.dart';
-import '../Modal/pixaBay_modal.dart';
+import 'package:pixabay_api/Modal/pixaBay_modal.dart';
 import '../api_helper.dart';
 
-class SearchProvider extends ChangeNotifier
-{
+int selectIndex = 0;
+
+class SearchProvider extends ChangeNotifier {
   Helper helper = Helper();
   SearchModal? searchModal;
   String search = '';
+  int selectIndex = 0;
 
-  void searchImg(String img)
-  {
+  void searchImg(String img) {
     search = img;
     notifyListeners();
   }
 
-  Future<SearchModal?> fromMap(String img)
-  async {
+  void selectedImage(int index) {
+    selectIndex = index;
+    notifyListeners();
+  }
+
+  Future<SearchModal?> fromMap(String img) async {
     final data = await helper.fetchApiData(img);
     searchModal = SearchModal.fromJson(data);
     return searchModal;
   }
-
 }
